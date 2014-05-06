0924ac27218a7264240ee5d40b528ea8b5cba5fc
nagstaku <nagstaku@gmail.com>
2014-05-05 23:06:25 -0700

    Test

---
+++ ContosoUniversity/DAL/SchoolContext.cs
@@ -0,0 +1,128 @@
+﻿using ContosoUniversity.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Threading.Tasks;
+
+namespace ContosoUniversity.DAL
+{
+    public class SchoolContext : DbContext
+    {
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Instructor> Instructors { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Person> People { get; set; }
+        public DbSet<OfficeAssignment> OfficeAssignments { get; set; }
+        public DbSet<ApplicationUser> Users { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+            modelBuilder.Entity<Course>()
+                .HasMany(c => c.Instructors).WithMany(i => i.Courses)
+                .Map(t => t.MapLeftKey("CourseID")
+                    .MapRightKey("InstructorID")
+                    .ToTable("CourseInstructor"));
+            modelBuilder.Entity<Department>().MapToStoredProcedures();
+        }
+    }
+
+    public class MyUserStore : IUserStore<ApplicationUser>, IUserPasswordStore<ApplicationUser>, IUserSecurityStampStore<ApplicationUser>
+    {
+        UserStore<IdentityUser> userStore = new UserStore<IdentityUser>(new SchoolContext());
+        
+        public Task CreateAsync(ApplicationUser user)
+        {
+            var context = userStore.Context as SchoolContext;
+            context.Users.Add(user);
+            context.Configuration.ValidateOnSaveEnabled = false;
+            return context.SaveChangesAsync();
+        }
+        public Task DeleteAsync(ApplicationUser user)
+        {
+            var context = userStore.Context as SchoolContext;
+            context.Users.Remove(user);
+            context.Configuration.ValidateOnSaveEnabled = false;
+            return context.SaveChangesAsync();
+        }
+        public Task<ApplicationUser> FindByIdAsync(string userId)
+        {
+            var context = userStore.Context as SchoolContext;
+            return context.Students.Users.Where(u => u.Id.ToLower() == userId.ToLower()).FirstOrDefaultAsync();
+        }
+        public Task<ApplicationUser> FindByNameAsync(string userName)
+        {
+            var context = userStore.Context as SchoolContext;
+            return context.Users.Where(u => u.UserName.ToLower() == userName.ToLower()).FirstOrDefaultAsync();
+        }
+        public Task UpdateAsync(ApplicationUser user)
+        {
+            var context = userStore.Context as SchoolContext;
+            context.Users.Attach(user);
+            context.Entry(user).State = EntityState.Modified;
+            context.Configuration.ValidateOnSaveEnabled = false;
+            return context.SaveChangesAsync();
+        }
+        public void Dispose()
+        {
+            userStore.Dispose();
+        }
+
+        public Task<string> GetPasswordHashAsync(ApplicationUser user)
+        {
+            var identityUser = ToIdentityUser(user);
+            var task = userStore.GetPasswordHashAsync(identityUser);
+            SetApplicationUser(user, identityUser);
+            return task;
+        }
+        public Task<bool> HasPasswordAsync(ApplicationUser user)
+        {
+            var identityUser = ToIdentityUser(user);
+            var task = userStore.HasPasswordAsync(identityUser);
+            SetApplicationUser(user, identityUser);
+            return task;
+        }
+        public Task SetPasswordHashAsync(ApplicationUser user, string passwordHash)
+        {
+            var identityUser = ToIdentityUser(user);
+            var task = userStore.SetPasswordHashAsync(identityUser, passwordHash);
+            SetApplicationUser(user, identityUser);
+            return task;
+        }
+        public Task<string> GetSecurityStampAsync(ApplicationUser user)
+        {
+            var identityUser = ToIdentityUser(user);
+            var task = userStore.GetSecurityStampAsync(identityUser);
+            SetApplicationUser(user, identityUser);
+            return task;
+        }
+        public Task SetSecurityStampAsync(ApplicationUser user, string stamp)
+        {
+            var identityUser = ToIdentityUser(user);
+            var task = userStore.SetSecurityStampAsync(identityUser, stamp);
+            SetApplicationUser(user, identityUser);
+            return task;
+        }
+        private static void SetApplicationUser(ApplicationUser user, IdentityUser identityUser)
+        {
+            user.PasswordHash = identityUser.PasswordHash;
+            user.SecurityStamp = identityUser.SecurityStamp;
+            user.Id = identityUser.Id;
+            user.UserName = identityUser.UserName;
+        }
+        private IdentityUser ToIdentityUser(ApplicationUser user)
+        {
+            return new IdentityUser
+            {
+                Id = user.Id,
+                PasswordHash = user.PasswordHash,
+                SecurityStamp = user.SecurityStamp,
+                UserName = user.UserName
+            };
+        }
+    }
+}
---
+++ ContosoUniversity/Models/ApplicationUser.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ContosoUniversity.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public ApplicationUser()
+        {
+            this.Id = Guid.NewGuid().ToString();
+        }
+        public ApplicationUser(string userName)
+            : this()
+        {
+            UserName = userName;
+        }
+        public virtual string Id { get; set; }
+        public virtual string PasswordHash { get; set; }
+        public virtual string SecurityStamp { get; set; }
+        public virtual string UserName { get; set; }
+    }
+}
